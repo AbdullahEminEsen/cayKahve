21b42c67d7da77ee74947488f6e4b7bae3bfa6da
Abdullah Emin Esen <esenabdullahemin@gmail.com>
2023-08-02 17:13:01 +0300

    User için Controller dosyası düzenlendi

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,106 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Office;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $users = User::orderBy('id','asc');
+        return view('users.index', compact('users'));
+    }
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index','show']] );
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'office_id' => 'required',
+            'order_id' => 'required',
+            'role' => 'required',
+        ]);
+        return redirect()->route('users.index')->with('success','User has been created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User  $user)
+    {
+        return view('users.show',compact('user'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User  $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User  $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'office_id' => 'required',
+            'order_id' => 'required',
+            'role' => 'required',
+        ]);
+
+        $user->fill($request->post())->save();
+
+        return redirect()->route('users.index')->with('success','User Has Been updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User  $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success','User has been deleted successfully');
+    }
 }
