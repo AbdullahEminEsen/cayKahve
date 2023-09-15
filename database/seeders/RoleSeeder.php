0b58faab050a1371f6eabc60841e5b1c0568d89e
Abdullah Emin Esen <esenabdullahemin@gmail.com>
2023-09-15 17:20:29 +0300

    proje ilk indirildiğinde kullanılması için role seeder oluşturuldu

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,7 +14,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name'=> 'admin']);
-        Role::create(['name'=> 'writer']);
-        Role::create(['name'=> 'user']);
+        Role::create(['name'=> 'cayci']);
+        Role::create(['name'=> 'kullanici']);
     }
 }
